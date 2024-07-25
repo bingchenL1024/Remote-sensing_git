0505ca06e379bde820102426f96bfec19227117c
bingchenL1024 <bil009@ucsd.edu>
2024-07-25 12:42:47 -0700

    first commit

--- Rectification/Toolbox/CoastalLens-main/CODES/helperFunctions/get_origin.m
+++
@@ -1,6 +0,0 @@
-%% get_origin
-function [origin_grid] = get_origin(Mopnum)
-
-
-load('/Users/bingchenliu/Documents/GitHub/Remote-sensing_code/Rectification/Toolbox/MopTableUTM.mat')
-origin_grid = [Mop.BackLat(Mopnum) Mop.BackLon(Mopnum) Mop.Normal(Mopnum)];
